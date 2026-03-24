96cc9500fbe604dc7971d0aba5f529cebea26e5e
MihajloPi - YT1GS <mihajlo.raspberrypi@gmail.com>
2026-03-24 09:25:15 +0100

    Refactor ST7789 header for C++

--- ST7789/st7789.h
+++ ST7789/st7789.h
@@ -1,266 +1,120 @@
-#ifndef __ST7789_H
-#define __ST7789_H
+#pragma once
 
-#include "fonts.h"
+#include "tft_fonts.h"
 #include "main.h"
+#include <cstdint>
+#include <cstddef>
 
-/* choose a Hardware SPI port to use. */
-#define ST7789_SPI_PORT hspi1
-extern SPI_HandleTypeDef ST7789_SPI_PORT;
+/* -----------------------------------------------------------------------
+ * RGB565 colour constants
+ * --------------------------------------------------------------------- */
+namespace Color {
+    constexpr uint16_t WHITE      = 0xFFFF;
+    constexpr uint16_t BLACK      = 0x0000;
+    constexpr uint16_t BLUE       = 0x001F;
+    constexpr uint16_t RED        = 0xF800;
+    constexpr uint16_t MAGENTA    = 0xF81F;
+    constexpr uint16_t GREEN      = 0x07E0;
+    constexpr uint16_t CYAN       = 0x07FF;   // fixed: was 0x7FFF
+    constexpr uint16_t YELLOW     = 0xFFE0;
+    constexpr uint16_t GRAY       = 0x8430;
+    constexpr uint16_t BROWN      = 0xBC40;
+    constexpr uint16_t DARKBLUE   = 0x01CF;
+    constexpr uint16_t LIGHTBLUE  = 0x7D7C;
+    constexpr uint16_t GRAYBLUE   = 0x5458;
+    constexpr uint16_t LIGHTGREEN = 0x07E4;   // fixed: was lavender 0x841F
+    constexpr uint16_t LGRAY      = 0xC618;
+    constexpr uint16_t LGRAYBLUE  = 0x5D1C;   // fixed: was sage green 0xA651
+    constexpr uint16_t LBBLUE     = 0x2B12;
+}
 
-/* choose whether use DMA or not */
-#define USE_DMA
-
-/* If u need CS control, comment below*/
-//#define CFG_NO_CS
-
-/* Pin connection*/
-#define ST7789_RST_PORT ST7789_RST_GPIO_Port
-#define ST7789_RST_PIN  ST7789_RST_Pin
-#define ST7789_DC_PORT  ST7789_DC_GPIO_Port
-#define ST7789_DC_PIN   ST7789_DC_Pin
-
-#ifndef CFG_NO_CS
-#define ST7789_CS_PORT  ST7789_CS_GPIO_Port
-#define ST7789_CS_PIN   ST7789_CS_Pin
-#endif
-
-/* If u need Backlight control, uncomment below */
-//#define BLK_PORT
-//#define BLK_PIN
-
-
-/*
- * Comment one to use another.
- * 3 parameters can be choosed
- * 135x240(0.96 inch) & 240x240(1.3inch) & 170x320(1.9inch)
- * X_SHIFT & Y_SHIFT are used to adapt different display's resolution
- */
-
-/* Choose a type you are using */
-//#define USING_135X240
-#define USING_240X240
-//#define USING_170X320
-
-/* Choose a display rotation you want to use: (0-3) */
-//#define ST7789_ROTATION 0
-//#define ST7789_ROTATION 1
-#define ST7789_ROTATION 2				//  use Normally on 240x240
-//#define ST7789_ROTATION 3
-
-#ifdef USING_135X240
-
-    #if ST7789_ROTATION == 0
-        #define ST7789_WIDTH 135
-        #define ST7789_HEIGHT 240
-        #define X_SHIFT 53
-        #define Y_SHIFT 40
-    #endif
-
-    #if ST7789_ROTATION == 1
-        #define ST7789_WIDTH 240
-        #define ST7789_HEIGHT 135
-        #define X_SHIFT 40
-        #define Y_SHIFT 52
-    #endif
-
-    #if ST7789_ROTATION == 2
-        #define ST7789_WIDTH 135
-        #define ST7789_HEIGHT 240
-        #define X_SHIFT 52
-        #define Y_SHIFT 40
-    #endif
-
-    #if ST7789_ROTATION == 3
-        #define ST7789_WIDTH 240
-        #define ST7789_HEIGHT 135
-        #define X_SHIFT 40
-        #define Y_SHIFT 53
-    #endif
-
-#endif
-
-#ifdef USING_240X240
-
-    #define ST7789_WIDTH 240
-    #define ST7789_HEIGHT 240
-
-		#if ST7789_ROTATION == 0
-			#define X_SHIFT 0
-			#define Y_SHIFT 80
-		#elif ST7789_ROTATION == 1
-			#define X_SHIFT 80
-			#define Y_SHIFT 0
-		#elif ST7789_ROTATION == 2
-			#define X_SHIFT 0
-			#define Y_SHIFT 0
-		#elif ST7789_ROTATION == 3
-			#define X_SHIFT 0
-			#define Y_SHIFT 0
-		#endif
-
-#endif
-
-#ifdef USING_170X320
-
-	#if ST7789_ROTATION == 0
-        #define ST7789_WIDTH 170
-        #define ST7789_HEIGHT 320
-        #define X_SHIFT 35
-        #define Y_SHIFT 0
-    #endif
-
-    #if ST7789_ROTATION == 1
-        #define ST7789_WIDTH 320
-        #define ST7789_HEIGHT 170
-        #define X_SHIFT 0
-        #define Y_SHIFT 35
-    #endif
-
-    #if ST7789_ROTATION == 2
-        #define ST7789_WIDTH 170
-        #define ST7789_HEIGHT 320
-        #define X_SHIFT 35
-        #define Y_SHIFT 0
-    #endif
-
-    #if ST7789_ROTATION == 3
-        #define ST7789_WIDTH 320
-        #define ST7789_HEIGHT 170
-        #define X_SHIFT 0
-        #define Y_SHIFT 35
-    #endif
-
-#endif
-
-/**
- *Color of pen
- *If you want to use another color, you can choose one in RGB565 format.
- */
-
-#define WHITE       0xFFFF
-#define BLACK       0x0000
-#define BLUE        0x001F
-#define RED         0xF800
-#define MAGENTA     0xF81F
-#define GREEN       0x07E0
-#define CYAN        0x7FFF
-#define YELLOW      0xFFE0
-#define GRAY        0X8430
-#define BRED        0XF81F
-#define GRED        0XFFE0
-#define GBLUE       0X07FF
-#define BROWN       0XBC40
-#define BRRED       0XFC07
-#define DARKBLUE    0X01CF
-#define LIGHTBLUE   0X7D7C
-#define GRAYBLUE    0X5458
-
-#define LIGHTGREEN  0X841F
-#define LGRAY       0XC618
-#define LGRAYBLUE   0XA651
-#define LBBLUE      0X2B12
-
-/* Control Registers and constant codes */
-#define ST7789_NOP     0x00
-#define ST7789_SWRESET 0x01
-#define ST7789_RDDID   0x04
-#define ST7789_RDDST   0x09
-
-#define ST7789_SLPIN   0x10
-#define ST7789_SLPOUT  0x11
-#define ST7789_PTLON   0x12
-#define ST7789_NORON   0x13
-
-#define ST7789_INVOFF  0x20
-#define ST7789_INVON   0x21
-#define ST7789_DISPOFF 0x28
-#define ST7789_DISPON  0x29
-#define ST7789_CASET   0x2A
-#define ST7789_RASET   0x2B
-#define ST7789_RAMWR   0x2C
-#define ST7789_RAMRD   0x2E
-
-#define ST7789_PTLAR   0x30
-#define ST7789_COLMOD  0x3A
-#define ST7789_MADCTL  0x36
-
-/**
- * Memory Data Access Control Register (0x36H)
- * MAP:     D7  D6  D5  D4  D3  D2  D1  D0
- * param:   MY  MX  MV  ML  RGB MH  -   -
+/* -----------------------------------------------------------------------
+ * ST7789 TFT driver
  *
- */
+ * Usage:
+ *   ST7789 tft(&hspi1,
+ *              ST7789_RST_GPIO_Port, ST7789_RST_Pin,
+ *              ST7789_DC_GPIO_Port,  ST7789_DC_Pin,
+ *              ST7789_CS_GPIO_Port,  ST7789_CS_Pin,  // nullptr/0 if no CS
+ *              240, 320);
+ *   tft.Init();
+ * --------------------------------------------------------------------- */
+class ST7789 {
+public:
+    /* DMA: minimum transfer size to bother using DMA over blocking SPI  */
+    static constexpr uint16_t DMA_MIN_SIZE = 16;
 
-/* Page Address Order ('0': Top to Bottom, '1': the opposite) */
-#define ST7789_MADCTL_MY  0x80
-/* Column Address Order ('0': Left to Right, '1': the opposite) */
-#define ST7789_MADCTL_MX  0x40
-/* Page/Column Order ('0' = Normal Mode, '1' = Reverse Mode) */
-#define ST7789_MADCTL_MV  0x20
-/* Line Address Order ('0' = LCD Refresh Top to Bottom, '1' = the opposite) */
-#define ST7789_MADCTL_ML  0x10
-/* RGB/BGR Order ('0' = RGB, '1' = BGR) */
-#define ST7789_MADCTL_RGB 0x00
+    /* DMA framebuffer strip height — increase if you have spare RAM      */
+    static constexpr uint16_t HOR_LEN = 5;
 
-#define ST7789_RDID1   0xDA
-#define ST7789_RDID2   0xDB
-#define ST7789_RDID3   0xDC
-#define ST7789_RDID4   0xDD
+    ST7789(SPI_HandleTypeDef *spi,
+           GPIO_TypeDef *rstPort, uint16_t rstPin,
+           GPIO_TypeDef *dcPort,  uint16_t dcPin,
+           GPIO_TypeDef *csPort,  uint16_t csPin,   // pass nullptr/0 for no CS
+           uint16_t width, uint16_t height,
+           uint16_t xShift = 0, uint16_t yShift = 0);
 
-/* Advanced options */
-#define ST7789_COLOR_MODE_16bit 0x55    //  RGB565 (16bit)
-#define ST7789_COLOR_MODE_18bit 0x66    //  RGB666 (18bit)
+    /* Core */
+    void Init();
+    void SetRotation(uint8_t m);
 
-/* Basic operations */
-#define ST7789_RST_Clr() HAL_GPIO_WritePin(ST7789_RST_PORT, ST7789_RST_PIN, GPIO_PIN_RESET)
-#define ST7789_RST_Set() HAL_GPIO_WritePin(ST7789_RST_PORT, ST7789_RST_PIN, GPIO_PIN_SET)
+    /* Basic drawing */
+    void FillColor(uint16_t color);
+    void DrawPixel(uint16_t x, uint16_t y, uint16_t color);
+    void DrawPixel4px(uint16_t x, uint16_t y, uint16_t color);
+    void Fill(uint16_t xSta, uint16_t ySta, uint16_t xEnd, uint16_t yEnd, uint16_t color);
 
-#define ST7789_DC_Clr() HAL_GPIO_WritePin(ST7789_DC_PORT, ST7789_DC_PIN, GPIO_PIN_RESET)
-#define ST7789_DC_Set() HAL_GPIO_WritePin(ST7789_DC_PORT, ST7789_DC_PIN, GPIO_PIN_SET)
-#ifndef CFG_NO_CS
-#define ST7789_Select() HAL_GPIO_WritePin(ST7789_CS_PORT, ST7789_CS_PIN, GPIO_PIN_RESET)
-#define ST7789_UnSelect() HAL_GPIO_WritePin(ST7789_CS_PORT, ST7789_CS_PIN, GPIO_PIN_SET)
-#else
-#define ST7789_Select() asm("nop")
-#define ST7789_UnSelect() asm("nop")
-#endif
+    /* Primitives */
+    void DrawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+    void DrawRectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+    void DrawFilledRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
+    void DrawCircle(uint16_t x0, uint16_t y0, uint8_t r, uint16_t color);
+    void DrawFilledCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color);
+    void DrawTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2,
+                      uint16_t x3, uint16_t y3, uint16_t color);
+    void DrawFilledTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2,
+                            uint16_t x3, uint16_t y3, uint16_t color);
 
-#define ABS(x) ((x) > 0 ? (x) : -(x))
+    /* Image & text */
+    void DrawImage(uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint16_t *data);
+    void WriteChar(uint16_t x, uint16_t y, char ch,
+                   TFT_FontDef font, uint16_t color, uint16_t bgcolor);
+    void WriteString(uint16_t x, uint16_t y, const char *str,
+                     TFT_FontDef font, uint16_t color, uint16_t bgcolor);
 
-/* Basic functions. */
-void ST7789_Init(void);
-void ST7789_SetRotation(uint8_t m);
-void ST7789_Fill_Color(uint16_t color);
-void ST7789_DrawPixel(uint16_t x, uint16_t y, uint16_t color);
-void ST7789_Fill(uint16_t xSta, uint16_t ySta, uint16_t xEnd, uint16_t yEnd, uint16_t color);
-void ST7789_DrawPixel_4px(uint16_t x, uint16_t y, uint16_t color);
+    /* Misc */
+    void InvertColors(bool invert);
+    void TearEffect(bool enable);
+    void Test();
 
-/* Graphical functions. */
-void ST7789_DrawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
-void ST7789_DrawRectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
-void ST7789_DrawCircle(uint16_t x0, uint16_t y0, uint8_t r, uint16_t color);
-void ST7789_DrawImage(uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint16_t *data);
-void ST7789_InvertColors(uint8_t invert);
+    uint16_t Width()  const { return _width;  }
+    uint16_t Height() const { return _height; }
 
-/* Text functions. */
-void ST7789_WriteChar(uint16_t x, uint16_t y, char ch, FontDef font, uint16_t color, uint16_t bgcolor);
-void ST7789_WriteString(uint16_t x, uint16_t y, const char *str, FontDef font, uint16_t color, uint16_t bgcolor);
+private:
+    SPI_HandleTypeDef *_spi;
+    GPIO_TypeDef      *_rstPort;  uint16_t _rstPin;
+    GPIO_TypeDef      *_dcPort;   uint16_t _dcPin;
+    GPIO_TypeDef      *_csPort;   uint16_t _csPin;   // nullptr = no CS
+    uint16_t _width, _height, _xShift, _yShift;
 
-/* Extented Graphical functions. */
-void ST7789_DrawFilledRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
-void ST7789_DrawTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3, uint16_t color);
-void ST7789_DrawFilledTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3, uint16_t color);
-void ST7789_DrawFilledCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color);
+    /* DMA framebuffer — HOR_LEN rows of pixels */
+    uint16_t _dispBuf[240 * HOR_LEN];   // sized for max supported width
 
-/* Command functions */
-void ST7789_TearEffect(uint8_t tear);
+    inline void Select()   const;
+    inline void UnSelect() const;
+    inline void DC_Set()   const;
+    inline void DC_Clr()   const;
+    inline void RST_Set()  const;
+    inline void RST_Clr()  const;
 
-/* Simple test function. */
-void ST7789_Test(void);
+    void WriteCommand(uint8_t cmd);
+    void WriteData(const uint8_t *buff, size_t size);
+    void WriteSmallData(uint8_t data);
+    void SetAddressWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
 
-#ifndef ST7789_ROTATION
-    #error You should at least choose a display rotation!
-#endif
-
-#endif
+    /* Byte-swap a RGB565 value for correct DMA wire order */
+    static constexpr uint16_t Swap16(uint16_t x) {
+        return static_cast<uint16_t>((x << 8) | (x >> 8));
+    }
+    static constexpr int16_t Abs(int16_t x) { return x >= 0 ? x : -x; }
+};
